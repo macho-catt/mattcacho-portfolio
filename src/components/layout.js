d30f8b5534e5a4c56edbc9ee52fa91629cc417ab
macho-catt <cacho.matthew@gmail.com>
2022-02-13 01:38:27 -0800

    Fix sidebar to left

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,10 @@ const Layout = ({ pageTitle, children }) => {
         <title>{pageTitle}</title>
       </head> */}
 
-      <div id="root" className="lg:grid lg:grid-cols-6">
+      <div id="root" className="lg:grid lg:grid-cols-6 lg:relative">
         <Nav />
-
+        {/* div to take the space of the fixed nav bar */}
+        <div className="lg:col-span-1" />
         <div className="lg:col-span-5">
           <main>
             <div className="flex flex-col justify-center bg-blue-100">
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import { navLinks } from '../config'
 
 const Nav = () => {
   return (
-    <nav className="lg:col-span-1 h-screen bg-midnightBlue text-white">
+    <nav className="lg:col-span-1 lg:fixed lg:top-0 lg:left-0 lg:box-border lg:w-1/6 h-screen z-1 bg-midnightBlue text-white">
 
       <ul className="lg:flex lg:flex-col lg:justify-center lg:gap-5 lg:h-full text-center">
         {
