110f1cd17c86e1579fb02592822c7090502c2183
macho-catt <cacho.matthew@gmail.com>
2022-02-22 14:20:27 -0800

    Modify resume button

--- src/styles/components/resume.styles.js
+++ src/styles/components/resume.styles.js
@@ -1,7 +1,16 @@
 const resumeStyles = {
   resume: [
+    'lg:mt-10',
+    'lg:border-solid',
+    'lg:border',
+    'lg:rounded-lg',
+    'lg:w-1/3',
+    'lg:h-12',
     'lg:text-center',
-    'lg:mt-5',
+    'lg:place-self-center',
+    'lg:flex',
+    'lg:flex-col',
+    'lg:justify-center',
     'hover:text-midnightBlue',
     'hover:bg-white',
   ].join(' '),
