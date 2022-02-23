d7cc74e4eb39f41e6d1ab368f98c42d7041a2de8
macho-catt <cacho.matthew@gmail.com>
2022-02-23 11:38:55 -0800

    Skills mobile

--- src/styles/components/sections/skills.styles.js
+++ src/styles/components/sections/skills.styles.js
@@ -1,36 +1,45 @@
 const skillStyles = {
   section: [
-    'lg:flex',
-    'lg:flex-col',
-    'lg:justify-center',
-    'lg:content-center',
-    'lg:h-screen',
-    'lg:w-full',
     'box-border',
+    'flex',
+    'flex-col',
+    'place-content-center',
+    'w-full',
+    'py-12',
+    'lg:pt-0',
+    'lg:pb-0',
+    'lg:h-screen',
   ].join(' '),
 
   container: [
-    'lg:flex',
-    'lg:flex-col',
-    'lg:justify-center',
+    'flex',
+    'flex-col',
+    'justify-center',
+    'gap-5',
+    'h-full',
+    'mx-10',
     'lg:gap-20',
-    'lg:h-full',
+    'lg:mr-0',
     'lg:ml-10',
   ].join(' '),
 
-  h2: ['lg:text-3xl', 'text-bumblebee'].join(' '),
+  h2: ['text-3xl', 'text-bumblebee', 'text-center', 'lg:text-left'].join(' '),
 
   body: [
+    'flex',
+    'flex-col',
+    'gap-5',
+    'text-center',
     'lg:flex',
     'lg:flex-row',
+    'lg:text-left',
     'lg:gap-28',
     'lg:h-1/3',
-    // 'bg-blue-500'
   ].join(' '),
 
-  col: ['lg:flex', 'lg:flex-col', 'lg:gap-2'].join(' '),
+  col: ['flex', 'flex-col', 'gap-2'].join(' '),
 
-  h3: ['lg:text-xl', 'text-teal'].join(' '),
+  h3: ['text-lg', 'lg:text-xl', 'text-teal'].join(' '),
 };
 
 export default skillStyles;
